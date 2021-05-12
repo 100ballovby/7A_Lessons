ab46e505d58fa9fd0b2d189522462f8c2b992097
demidraksin <greatraksin@icloud.com>
2021-05-12 14:10:01 +0300

    Adding digital clock lesson🍕

---
+++ IV_term/04_lesson_1205/script.js
@@ -0,0 +1,24 @@
+function currentTime() {
+    let date = new Date(); // создал объект даты
+    let hours = date.getHours();
+    hours = (hours === 0) ? 12 : ((hours > 12) ? (hours - 12) : hours)
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    hours = updateTime(hours);
+    minutes = updateTime(minutes);
+    seconds = updateTime(seconds);
+
+    let midday = 'AM';
+    midday = (hours >= 12) ? 'AM':'PM';
+
+    document.getElementById('clock').innerHTML =
+        hours + ':' + minutes + ':' + seconds + ' ' + midday;
+    let t = setTimeout(function(){currentTime()}, 1000);
+}
+
+function updateTime(k) {
+    if (k < 10) {
+        return '0' + k;  // чтобы было не 13:5:2, а 13:05:02
+    } else return k;
+}
+currentTime()
